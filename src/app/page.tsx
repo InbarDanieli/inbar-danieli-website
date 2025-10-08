29740bea27e67e7c525625ecee22225891813442
Inbar Danieli <inbar0308@gmail.com>
2025-10-08 23:49:09 +0300

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,47 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
+import githubIcon from "../assets/github-icon.svg";
 
 export default function Home() {
+  const socials = [
+    {
+      name: "GitHub",
+      url: "https://github.com/inbardanieli",
+      icon: (
+   <></>
+      ),
+    },
+  ];
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
+      <Image
+        className={styles["cover-image"]}
+        alt=""
+        src="/inbar-danieli.png"
+        width={160}
+        height={160}
+      />
+      <h1>Inbar Danieli</h1>
+      <h3>FrontEnd Developer</h3>
+      <div className={styles.socials}>
+        {socials.map((social) => (
           <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles["social-link"]}
+            key={social.name}
+            href={social.url}
             target="_blank"
             rel="noopener noreferrer"
+            aria-label={social.name}
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            {social.icon}
           </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        ))}
+      </div>
+      <h5>
+        FrontEnd Developer and open source contributor. A self-learner, always
+        eager to explore new technologies, and continuously improve my skills.
+        CSS enthusiast and responsible for developing the Reversim 2025 website.
+      </h5>
     </div>
   );
 }
